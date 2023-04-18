d6845f67d181c53b9c1c42ed4fcef1dfb85ae5bd
maxai <maxai@MAX>
2023-04-18 20:05:37 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 1, 2, 3 };
+            Console.WriteLine(add(number));
+        }
+
+        static int add(int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
